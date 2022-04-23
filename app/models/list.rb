c87cc2ad6d76b1e508fafb1326420cd9abe31c95
Mark Soosaipillai <markbrian14@hotmail.com>
2022-04-23 16:35:58 -0400

    added images to input

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,7 @@
 class List < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks
+  has_one_attached :image
+
+  validates :name, presence: true, uniqueness: true
 end
